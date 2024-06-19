7dccfae87a31720d0685d98d6a4e1d9f94453886
Nejc Krmelj <nejc.krmelj.id@gmail.com>
2024-06-20 00:16:51 +0200

    feat: auto create mosquitto user

---
+++ software/mosquitto/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/ash
+
+set -e
+
+# Fix write permissions for mosquitto directories
+chown --no-dereference --recursive mosquitto /mosquitto/
+
+mkdir -p /var/run/mosquitto \
+  && chown --no-dereference --recursive mosquitto /var/run/mosquitto
+
+
+if ( [ -z "${MQTT_USERNAME}" ] || [ -z "${MQTT_PASSWORD}" ] ); then
+  echo "MQTT_USERNAME or MQTT_PASSWORD not defined"
+  exit 1
+fi
+
+# create mosquitto passwordfile
+echo "Adding user $MQTT_USERNAME"
+touch passwordfile
+chown mosquitto /passwordfile
+chgrp mosquitto /passwordfile
+chmod 0600 /passwordfile
+mosquitto_passwd -b passwordfile $MQTT_USERNAME $MQTT_PASSWORD
+
+exec "$@"
